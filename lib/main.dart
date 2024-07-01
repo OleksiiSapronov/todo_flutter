0deacdab74dc83dd250fdc6502d929788c47aadf
OleksiiSapronov <oleksiisapronov62@gmail.com>
2024-07-01 04:46:22 +0100

    Modified using dotenv & MVVM

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,23 @@
 import 'package:flutter/material.dart';
-import 'my_app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:my_todo_app/view/todos_list.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter TODO App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TodosList(title: 'TODO List'),
+    );
+  }
+}
